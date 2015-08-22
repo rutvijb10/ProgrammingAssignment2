ce7173ce5dc3aee5839ea20f82d9b2a78d900928
rutvij bora <rutvijb10@live.com>
2015-08-22 22:58:06 +0530

    Code Addition
    Writing code for the first time in the R file for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  i <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  setInverseMatrix <- function(solve) i <<- solve
+  getInverseMatrix <- function() i
+  
+  list(set = set , get = get , setInverseMatrix = setInverseMatrix , getInverseMatrix = getInverseMatrix)
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  i<-x$getInverseMatrix()
+  if(!is.null(i))
+  {
+    message("getting cached data")
+    return(i)
+  }
+  #message("Not cached")
+  matrix <- x$get()
+  i <- solve(matrix, ...)
+  x$setInverseMatrix(i)
+  i
 }
